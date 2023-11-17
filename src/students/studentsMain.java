d4a096db5ea63b22a5bd14571c246607b0320766
Joshua <joshuaquidit123456@gmail.com>
2023-11-17 15:36:04 +0800

    Re-added students

---
+++ src/students/studentsMain.java
@@ -0,0 +1,13 @@
+package students;
+
+class StudentsMain {
+    
+    public static void main(String[] args){
+        
+        Students student = new Students ("Ariel S. Sumantin", "IT21C", "Delete System32 but in real-life");
+        
+        System.out.println("Name: " + student.getName());
+        System.out.println("Section: " + student.getSection());
+        System.out.println("Motto: " + student.getMotto());
+    }
+}
