810d4de8711cff2c25ecc711bb3163fb5ab6af9d
hershey <hershey@gmail.com>
2023-11-23 16:41:36 +0800

    Added @Override

--- src/polymorphism/Polymorphism_Ariel.java
+++ src/polymorphism/Polymorphism_Ariel.java
@@ -4,9 +4,11 @@ public class Polymorphism_Ariel extends Polymorphism_Student{ //child class
     Polymorphism_Ariel(String name){
         super(name);
     }
+    @Override
     void sleep(){
         System.out.println(name + " kay gatulog nga gasiga ang mata");
     }
+    @Override
     public void studentCute(){
         System.out.println("Nagpa CUTE si " + name);
     }
--- src/polymorphism/Polymorphism_Zyrel.java
+++ src/polymorphism/Polymorphism_Zyrel.java
@@ -4,9 +4,11 @@ public class Polymorphism_Zyrel extends Polymorphism_Student{   //child class
     Polymorphism_Zyrel(String name){
         super(name);
     }
+    @Override
     void coding(){
         System.out.println(name + " kay galabad ang ulo sa sigeg code");
     }    
+    @Override
     public void studentCute(){
         System.out.println("Nagpa CUTE si " + name);
     }
